9524fab4e57090fc5eb5ad6ef0d22a9901bb90a9
jovanni-80 <jovanni.rodriguez@yahoo.com>
2025-08-25 08:56:55 -0400

    added terminal logging

--- debian_post_install.sh
+++ debian_post_install.sh
@@ -6,16 +6,20 @@
 # Note: sudo should be setup before this ideally
 #
 
+error_prefix='\033[41m ERROR \033[0m'
+info_prefix='\033[42m INFO  \033[0m'
+warn_prefix='\033[43m WARN  \033[0m'
+
 set expected_user="whitakeradm"
 set expected_hostname="homelab"
 set WIREGUARD_PORT="51820"
 
 # script should not be executable by non-sudo user
 if [[ "$USER" != "$expected_user" ]]; then
-  echo "User $expected_user needs to run this script with sudo!"
+  echo -e "$error_prefix User $expected_user needs to run this script with sudo!"
   exit 1
 elif [ "$EUID" -ne 0 ]; then
-  echo "Please run with 'sudo'"
+  echo -e "$error_prefix Please run with 'sudo'"
   echo "If you have not, first run: "
   echo "$ sudo apt update && sudo apt install sudo -y"
   echo "$ su"
@@ -24,12 +28,16 @@ elif [ "$EUID" -ne 0 ]; then
 fi
 
 # PACKAGE INSTALLATIONS:
+echo -e "$info_prefix Installing apt packages"
+sleep 1
+
 sudo apt update -y
 sudo apt install timeshift -y
 sudo apt install openssh-server -y
 sudo apt install neovim vim -y
 sudo apt install ufw -y
 sudo apt install git -y
+sudo apt install tmux -y
 sudo apt install build-essential -y
 sudo apt install unzip -y
 sudo apt install fish -y
@@ -44,6 +52,8 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install python3 -y
 
+echo -e "$info_prefix Adding Docker GPG key and installing"
+sleep 1
 # docker install
 # Add Docker's official GPG key:
 sudo apt-get update
@@ -64,6 +74,8 @@ sudo docker run hello-world
 # @TODO: add dockerfile emplacement gitea, wireguard, dns/pihole maybe, minecraft server
 #
 
+echo -e "$info_prefix Installing nerdfont"
+sleep 1
 # install nerdfonts - don't do this as root probably?
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip &&
   cd ~/.local/share/fonts &&
@@ -71,26 +83,35 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
   rm JetBrainsMono.zip &&
   fc-cache -fv
 
+echo -e "$info_prefix installing oh-my-fish"
+sleep 1
 # install oh my fish - also don't do this as root probably
 curl -L https://get.oh-my.fish | fish
 
+echo -e "$info_prefix installing uv"
+sleep 1
 # install 'uv' for python projects
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+echo -e "$info_prefix configuring timeshift to backup weekly"
+sleep 1
 # configure timeshift backups, sudo
 sed -i 's/"schedule_weekly": false/"schedule_weekly": true/' /etc/timeshift/timeshift.json
 
+echo -e "$info_prefix configuring uncomplicated firewall"
+sleep 1
+
 # configure (uncomplicated) firewall
-ufw default deny incoming
-ufw default allow outgoing
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
 
-ufw allow ssh
+sudo ufw allow ssh
 
-ufw allow $WIREGUARD_PORT/udp
+sudo ufw allow $WIREGUARD_PORT/udp
 
-echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf
+sudo echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf
 # If using IPv6:
-echo 'net.ipv6.conf.all.forwarding=1' | sudo tee -a /etc/sysctl.conf
+sudo echo 'net.ipv6.conf.all.forwarding=1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 # need to make sure this exists in /etc/ufw/before.rules
@@ -109,16 +130,18 @@ sudo sysctl -p
 #-A ufw-before-forward -i wg0 -j ACCEPT
 #-A ufw-before-forward -o wg0 -j ACCEPT
 
-ufw enable
+sudo ufw enable
 
+echo -e "$info_prefix Configuring fish"
+sleep 1
 # configure some aliases
-echo 'alias reboot "/sbin/reboot"' >~/.config/fish/config.fish
-echo 'alias src "source /home/$expected_user/.config/fish/config.fish"' >~/.config/fish/config.fish
-echo 'alias f "nvim /home/$expected_user/.config/fish/config.fish"' >~/.config/fish/config.fish
+echo 'alias reboot "/sbin/reboot"' > ~/.config/fish/config.fish
+echo 'alias src "source /home/$expected_user/.config/fish/config.fish"' > ~/.config/fish/config.fish
+echo 'alias f "nvim /home/$expected_user/.config/fish/config.fish"' > ~/.config/fish/config.fish
+
+# @TODO: add fish configuration emplacement
 
 # set fish as default shell
 chsh -s $(which fish)
 
-# @TODO: add fish configuration emplacement
 # @TODO: add tmux configuration emplacement
-
